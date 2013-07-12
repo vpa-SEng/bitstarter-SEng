adcaad97a1526478f9c3967fcdf4a730637f0adf
vpa-SEng <v.pangaldus@gmail.com>
2013-07-13 00:21:58 +0200

    Test fix

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var bufPage = new Buffer(256);
+  var bufPage = new Buffer(8192);
   bufPage = fs.readFileSync("index.html"); 
-  response.send(bufPage.toString('utf-8', 0, 28));
+  response.send(bufPage.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
